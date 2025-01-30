a9d42bb7eff28ffb9824c712c7ba9ee520e03eab
eplik82 <alvar.kutti@gmail.com>
2025-01-30 15:53:20 +0200

    NewApp

---
+++ EsimeneProjekt/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Sisesta nimi: ");
+string userName;
+userName = Console.ReadLine();
+Console.WriteLine("Tere " + userName + "!");
+Console.WriteLine($"Tere, {userName} !"); 
