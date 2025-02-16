26c25afd914dc4526fb29903bc5f78601f3614eb
Alvar Kutti <alvar.kutti@tktk.ee>
2025-02-16 15:19:46 +0200

    week2

---
+++ Week2-Vanus/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Sisesta vanus:");
+
+String userAge = Console.ReadLine();
+int userAgeNum = 0;
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+
+if (isAgeNumber == true)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Teretulemast lehele!");
+    }
+    else
+    {
+        Console.WriteLine("Kahjuks liiga noor");
+
+    }
+}
+else
+{
+    Console.WriteLine("Sisestage number");
+}
--- Week2/Program.cs
+++ Week2/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Sisesta oma sugu m/n:");
+using System.Runtime.Intrinsics.X86;
 
+Console.WriteLine("Sisesta oma sugu m/n:");
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Palun sisestage oma perenimi:");
+String userLastName = Console.ReadLine();
+
+if(userGender == 'm')
+{
+    Console.WriteLine($"Tere Mr. {userLastName}!");
+}
+else if(userGender == 'n')
+{
+    Console.WriteLine($"Tere Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Tere {userLastName}!");
+}
