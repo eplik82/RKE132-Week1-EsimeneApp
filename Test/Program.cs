a4d47a965727dcd94c45336ae4f8adc9980e9485
Alvar Kutti <alvar.kutti@tktk.ee>
2025-05-04 22:22:45 +0300

    week8

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,32 @@
-﻿string[] colors = { "red", "green", "blue", "pink", "yellow" };
-for (int i = colors.Length - 1; i == 0; i--)
+﻿
+
+Console.WriteLine("Enter conversion method, to Farenheit or Celsius (f/c)");
+char userOp = char.Parse(Console.ReadLine());
+
+
+switch (userOp)
 {
-    Console.WriteLine(colors[i]);
+    case 'c':
+        Console.WriteLine("Enter temp in Farenheits:");
+        int Far = int.Parse(Console.ReadLine());
+        ConverToCelsius(Far);
+        break;
+    case 'f':
+        Console.WriteLine("Enter temp in Celsius:");
+        int Cel = int.Parse(Console.ReadLine());
+        ConvertToFahrenheit(Cel);
+        break;
+    default:
+        Console.WriteLine("invalid operation, enter f or c");
+        break;
+}
+static void ConverToCelsius(int temp)
+{   int cel = (temp -32) * 5/9;
+    Console.WriteLine($"{temp} Farenheits are {cel} in Celsius");
+}
+
+static void ConvertToFahrenheit(int temp)
+{
+    int far = (temp * 9) / 5 + 32;
+    Console.WriteLine($"{temp} Celsius are {far} in Farenheits");
 }
---
+++ week8-EpicGame/Program.cs
@@ -0,0 +1,85 @@
+﻿//string[] heros = {"Harry Potter","Lara Craft","Luke Skywalker","Scooby-Doo"};
+//string[] villains = { "Voldemort", "Darth Vader", "Dracula", "Joker", "Sauron" };
+string folderPath = @"C:\TKTK\";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heros = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+string[] weapons = { "magic wand", "plastic fork", "banana", "wooden sword", "Lego brick" };
+
+string hero = GetRandom(heros);
+string heroWeapon = GetRandom(weapons);
+int heroHP = GetCharacterHP(hero);
+int heroStrikeStrength = heroHP;
+Console.WriteLine($"Your hero is {hero} ({heroHP} HP), with {heroWeapon}");
+
+string villain = GetRandom(villains);
+string villiainWeapon = GetRandom(weapons);
+int villainHP = GetCharacterHP(villain);
+int villainStrikeStrength = villainHP;
+Console.WriteLine($"Your Villain is {villain} ({villainHP} HP) with {villiainWeapon}");
+
+while(heroHP > 0 && villainHP > 0)
+{
+    heroHP = heroHP - Hit(villain, villainStrikeStrength);
+    villainHP = villainHP - Hit(hero, heroStrikeStrength);
+}
+
+Console.WriteLine($"Hero {hero} HP: {heroHP}");
+Console.WriteLine($"Villain {villain} HP: {villainHP}");
+
+if (heroHP > 0)
+{
+    Console.WriteLine($"{hero} saved the day!");
+}
+else if(villainHP > 0)
+{
+    Console.WriteLine($"{villain} destroyed the day!");
+}
+else
+{
+    Console.WriteLine("Draw!");
+}
+
+static string GetRandom(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
+
+static int GetCharacterHP(string characterName)
+{
+    if (characterName.Length < 10)
+    {
+        return 10;
+    }
+    else
+    {
+        return characterName.Length;
+    }
+}
+
+static int Hit(string charName, int characterHP)
+{
+    Random rnd = new Random();
+    int strike = rnd.Next(0, characterHP);
+
+    if (strike == 0)
+    {
+        Console.WriteLine($"Your character {charName} missed the target!");
+    }
+    else if (strike == characterHP -1) 
+    {
+        Console.WriteLine($" {charName} made a critical hit!");
+    }
+    else
+    {
+        Console.WriteLine($" {charName} hit {strike}!");
+    }
+
+    return strike;
+}
