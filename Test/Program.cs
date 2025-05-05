38f2c7e462289ca190894c1ee62bc5dc6cafc51f
Alvar Kutti <alvar.kutti@tktk.ee>
2025-05-05 20:40:24 +0300

    week9

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,32 +1,10 @@
-﻿
+﻿string path = @"C:\TKTK";
+string fileName = "chuck.txt";
+string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+Console.WriteLine(GetJoke(lines));
 
-Console.WriteLine("Enter conversion method, to Farenheit or Celsius (f/c)");
-char userOp = char.Parse(Console.ReadLine());
-
-
-switch (userOp)
+static string GetJoke(string[] jokeArray)
 {
-    case 'c':
-        Console.WriteLine("Enter temp in Farenheits:");
-        int Far = int.Parse(Console.ReadLine());
-        ConverToCelsius(Far);
-        break;
-    case 'f':
-        Console.WriteLine("Enter temp in Celsius:");
-        int Cel = int.Parse(Console.ReadLine());
-        ConvertToFahrenheit(Cel);
-        break;
-    default:
-        Console.WriteLine("invalid operation, enter f or c");
-        break;
-}
-static void ConverToCelsius(int temp)
-{   int cel = (temp -32) * 5/9;
-    Console.WriteLine($"{temp} Farenheits are {cel} in Celsius");
-}
-
-static void ConvertToFahrenheit(int temp)
-{
-    int far = (temp * 9) / 5 + 32;
-    Console.WriteLine($"{temp} Celsius are {far} in Farenheits");
+    Random rnd = new Random();
+    return jokeArray[rnd.Next(9, jokeArray.Length)];
 }
